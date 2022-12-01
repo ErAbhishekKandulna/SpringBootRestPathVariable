9ee8cc0da93a6670ca0377c34dfdc8b7a5e0f61a
ErAbhishekKandulna <abhishekkandulna1999@gmail.com>
2022-12-01 17:17:10 +0530

    Spring Boot Rest Path Variable

---
+++ src/main/java/com/app/raghu/controller/ProductRestController.java
@@ -0,0 +1,61 @@
+package com.app.raghu.controller;
+
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductRestController 
+{
+	//using requestparam
+	//.../dataa?pid=10&pname=A
+	@GetMapping("/dataa")
+	public ResponseEntity<String> showDataA(@RequestParam("pid")Integer id,
+			@RequestParam("pname")String name)
+	{
+		System.out.println("PID "+ id +" PNAME "+ name);
+		return new ResponseEntity<>("CHECK AT CONSOLE",HttpStatus.OK);
+	}
+	
+	//using pathvariable
+	//.../datab/10/A
+	@GetMapping("/datab/{pid}/{pname}")
+	public ResponseEntity<String> showDataB(@PathVariable("pid")Integer id,@PathVariable("pname")String name)
+	{
+		System.out.println("PID "+ id +" PNAME "+ name);
+		return new ResponseEntity<>("CHECK YOUR CONSOLE",HttpStatus.OK);
+	}
+	
+	@PostMapping("/details")
+	public ResponseEntity<String> showHeaders(
+			@RequestHeader("Content-Type")String type,
+			@RequestHeader("Content-Length")String len,
+			@RequestHeader("Authorization")String auth
+			)
+	{
+		System.out.println(auth);
+		System.out.println("data "+type+" - "+len);
+		return new ResponseEntity<>("CHECK",HttpStatus.OK);
+	}
+	
+	public ResponseEntity<?> showInfo()
+	{
+		if(new Random().nextInt() > 0)
+		{
+			return new ResponseEntity<>("OK",HttpStatus.OK);
+		}
+		else
+		{
+			return new ResponseEntity<>(101,HttpStatus.OK);
+		}
+	}
+}
